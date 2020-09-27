9b4d1630430ce4c4082afcd3fb2fdc7493edb56f
Taku-kon <kimikoe.reki.zoe@gmail.com>
2020-09-27 15:27:54 +0900

    ユーザー管理機能
    アプリケーションコントローラーの編集。ragistrationファイルのnewの編集
    ヘッダーリンクの訂正
    routesの編集

--- db/migrate/20200918101725_devise_create_users.rb
+++ db/migrate/20200918101725_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.date :birthday,             null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :first_kana,         null: false
+      t.string :last_kana,          null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -35,7 +41,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
       t.timestamps null: false
     end
-
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
