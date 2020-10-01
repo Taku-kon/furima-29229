00251f317d9af082a9f8b22a31b3a9d8c4807f14
Taku-kon <kimikoe.reki.zoe@gmail.com>
2020-10-01 12:55:54 +0900

    余分な部分の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,24 +4,14 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @Item = User.new
   end
 
   def create
-    @Item = User.create(user_params)
   end
 
   def edit
-    
   end
 
   def show
-    
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit( :email, :password, :nickname, :birthday, :first_name, :last_name, :first_kana, :last_kana,)
   end
 end
