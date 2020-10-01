0e0ab875ac8af3e81a51b941b6482ebefd8a7b81
Taku-kon <kimikoe.reki.zoe@gmail.com>
2020-10-01 13:24:23 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :password, length: { minimum: 6 } format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :email, uniqueness: true, format: {with:  /\A\S+@\S+\.\S+\z/ }
+    NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+    KANA_REGEX = /\A[ァ-ヶー－]+\z/
+    validates :last_name, format: {with: NAME_REGEX }
+    validates :last_kana, format: {with: KANA_REGEX }
+    validates :first_name, format: {with: NAME_REGEX }
+    validates :first_kana, format: {with: KANA_REGEX }
+  end
 end
