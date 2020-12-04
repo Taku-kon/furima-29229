e2a24283771d0894f777bd4ed796ce4cacb8b47d
Taku-kon <kimikoe.reki.zoe@gmail.com>
2020-12-04 12:11:28 +0900

    修正
    URLを直接入力して購入済み商品の購入ページへ遷移しようとすると、トップページに遷移する。実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,14 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :dateparams, only: [:index, :create]
   before_action :owner_kick
+  before_action :direct_guard
 
   def index
-    @order = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @order = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_sys
@@ -35,10 +35,20 @@ class OrdersController < ApplicationController
   end
 
   def owner_kick
-    @order = Item.find(params[:item_id])
+    dateparams
     if @order.user.id == current_user.id
       redirect_to root_path
     end
+  end
+
+  def direct_guard
+    if @order.order.present?
+      redirect_to root_path
+    end
+  end
+
+  def dateparams
+    @order = Item.find(params[:item_id])
   end
 
 end
